50edd6585b766e7b6e70496d31b8aa4023e15d98
Frank San Filippo <sanflip77@gmail.com>
2017-09-21 12:33:21 -0500

    Added a websocket connetion with template

--- index.js
+++ index.js
@@ -15,12 +15,43 @@ app.get('/',function(req,res){
   res.sendFile(path.join(__dirname + '/views/helloWord.html'));
 });
 
-app.get('/collections/:name',function(req,res){
+/**app.get('/collections/:name',function(req,res){
   var collection = db.get(req.params.name);
   collection.find({},{limit:20},function(e,docs){
     res.json(docs);
   })
+});**/
+
+var WebSocketServer = require('websocket').server;
+var http = require('http');
+
+var server = http.createServer(function(request, response) {
+  // process HTTP request. Since we're writing just WebSockets
+  // server we don't have to implement anything.
 });
 
+server.listen(1337, function() { });
+
+// create the server
+wsServer = new WebSocketServer({
+  httpServer: server
+});
+
+// WebSocket server
+wsServer.on('request', function(request) {
+  var connection = request.accept(null, request.origin);
+
+  // This is the most important callback for us, we'll handle
+  // all messages from users here.
+  connection.on('message', function(message) {
+    if (message.type === 'utf8') {
+      // process WebSocket message
+    }
+  });
+
+  connection.on('close', function(connection) {
+    // close user connection
+  });
+});
 
 app.listen(3000)
